3a8857635176a2bf445c6f128a943778d908ee76
Vladimirs Nordholm <v@vladde.net>
2020-12-12 21:21:08 +0100

    add option --from-date=YYYY-MM-DD

--- extract.php
+++ extract.php
@@ -2,9 +2,31 @@
 
 require "vendor/autoload.php";
 
+use JetBrains\PhpStorm\ArrayShape;
 use PHPHtmlParser\Dom;
 
-$dom = new Dom;
+#[ArrayShape(['fromDate' => 'DateTimeImmutable|null'])]
+function getOptions(): array
+{
+    $options = getopt('', ['from-date::']);
+    $fromDate = null;
+
+    if ($options['from-date'] ?? false) {
+        try {
+            $fromDate = new DateTimeImmutable($options['from-date']);
+        } catch (Exception) {
+            echo "Invalid date '{$options['from-date']}'";
+        }
+    }
+
+    return [
+        'fromDate' => $fromDate
+    ];
+}
+
+$options = getOptions();
+
+$dom = new Dom();
 $dom->loadFromFile('kontotransactionlist.xls');
 $rows = $dom->find('form > table')[3]->find('tr')->toArray();
 array_shift($rows);
@@ -14,7 +36,7 @@ foreach ($rows as $row) {
     $values = $row->find('td');
 
     $clearedDate = $values[0]->text;
-    $transactionDate = $values[2]->text;
+    $transactionDate = new DateTimeImmutable($values[2]->text);
     $note = utf8_encode(html_entity_decode($values[4]->text));
     $delta = $values[6]->text;
 
@@ -22,9 +44,13 @@ foreach ($rows as $row) {
         $clearedDate = '';
     }
 
+    if($options['fromDate'] && $transactionDate < $options['fromDate']) {
+        continue;
+    }
+
     $delta = str_replace([' ', ','], ['', '.'], $delta);
 
-    $output .= "$transactionDate,,\"" . str_replace('"', '\"', $note) . "\",$delta\n";
+    $output .= "{$transactionDate->format('Y-m-d')},,\"" . str_replace('"', '\"', $note) . "\",$delta\n";
 }
 
 file_put_contents('ynab.csv', $output);
